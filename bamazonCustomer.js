02c4b2fb38ab1a76683ea32905935c3ee23aad24
Magus <mfail792@gmail.com>
2019-09-28 13:27:42 -0500

    updated queryAll function to include first inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,29 +24,28 @@ connection.connect(function (err) {
 });
 
 function queryAll() {
-    inquirer
-        .prompt({
-            name: "firstQ",
-            type: "list",
-            message: "Here are the items for sale: ",
-        })
-        .then(displayTable())
-
-}
-
-function displayTable() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
+
+
         console.log("-----------------------------------");
+
+        inquirer.prompt({
+            name: "firstQ",
+            type: "input",
+            message: "What is the ID of the item you would like to purchase? "
+        }, {
+            name: "secondQ",
+            type: "input",
+            message: "How many would you like? "
+        })
+
+
     });
 }
-
-
-
-
 
 
 
