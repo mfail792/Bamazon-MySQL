20fc1147773c84017c4f623d4b5d9368de0ddb57
Magus <mfail792@gmail.com>
2019-09-29 23:11:22 -0500

    updated queryAll

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,22 +11,68 @@ var connection = mysql.createConnection({
 })
 connection.connect();
 
-
-var display = function() {
-    connection.query("SELECT * FROM products", function(err, res) {
+var displayAll = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("---------------------------------");
         console.log("        Welcome to Bamazon!      ");
         console.log("---------------------------------");
         console.log("");
-        console.log("Choose Your Product Below");
+        console.log("Choose Your Product Below:");
         console.log("");
 
-});
+        //cli table documentation with alignments
+        var table = new Table({
+            head: ["Item ID", "Product Name", "Dept.", "Price", "Quantity"],
+            colWidths: [13, 45, 7],
+            colAligns: ["center", "left", "right"],
+            style: {
+                head: ["green"],
+                compact: true
+            }
+        });
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        console.log("");
+        queryAll();
 
-var table = new Table({
-    head: ['Item ID', 'Product Name', 'Price']
-  , colWidths: [13, 45, 7]
-});
+    });
+}
+
+function queryAll() {
+    connection.query("SELECT * FROM products", function (err) {
+        if (err) throw err;
+        console.log("-----------------------------------");
+        inquirer.prompt([{
+            name: "itemID",
+            type: "input",
+            message: "What is ID of the item you'd like to buy?"
+        }, {
+            name: "unitAmount",
+            type: "input",
+            message: "How many units of the item would you like?"
+        }])
+            .then(function (answer) {
+                var query = "SELECT * FROM products where ?"
+                connection.query(query, { item_id: answer.itemID }, function (err, res) {
+                    if (err) throw err;
+                    if (res.length === 0) {
+                        console.log("Invalid product number")
+                    };
+                    console.log("Success!");
+                    connection.end();
+                });
+            });
+    });
+
+
+}
+
+displayAll();
+
+
+
 
 
